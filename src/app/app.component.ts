a24a37f35abe1d287e8ce6f8af453822752c80ca
Christoph <c.wagner.germany@gmail.com>
2024-04-05 18:45:24 +0200

    user-interface works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserInterfaceComponent } from './user-interface/user-interface.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserInterfaceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
