690955a8596987568fe64b85b504ed0780af61ff
Christoph <c.wagner.germany@gmail.com>
2024-04-05 19:29:50 +0200

    form working

--- src/app/user-interface/user-interface.component.ts
+++ src/app/user-interface/user-interface.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-interface',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-interface.component.html',
   styleUrl: './user-interface.component.scss'
 })
-export class UserInterfaceComponent {
+export class UserInterfaceComponent implements AfterViewInit {
+  userInput?: number;
+  @ViewChild('inputField') inputField!: ElementRef;
 
+  ngAfterViewInit() {
+    this.inputField.nativeElement.focus();
+  }
+
+  onInputChange() {
+    console.log("User input changed to: ", this.userInput);
+  }
 }
