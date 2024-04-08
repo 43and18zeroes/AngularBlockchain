77606f48dbbae6ed57656e980765672dee505645
Christoph <c.wagner.germany@gmail.com>
2024-04-08 19:32:34 +0200

    child components

--- src/app/user-interface/user-interface.component.ts
+++ src/app/user-interface/user-interface.component.ts
@@ -7,9 +7,9 @@ import {
   ViewChild,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { UserChoiceFormComponent } from '../user-choice-form/user-choice-form.component';
-import { AddTransactionFormComponent } from '../add-transaction-form/add-transaction-form.component';
-import { OutputBlockchainComponent } from '../output-blockchain/output-blockchain.component';
+import { UserChoiceFormComponent } from './user-choice-form/user-choice-form.component';
+import { AddTransactionFormComponent } from './add-transaction-form/add-transaction-form.component';
+import { OutputBlockchainComponent } from './output-blockchain/output-blockchain.component';
 
 @Component({
   selector: 'app-user-interface',
