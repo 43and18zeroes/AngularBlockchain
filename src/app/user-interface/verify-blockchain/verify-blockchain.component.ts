e5c1b8819f5be681604c5c33e195208c2ac58b07
Christoph <c.wagner.germany@gmail.com>
2024-04-10 19:00:01 +0200

    verify blockchain

--- src/app/user-interface/verify-blockchain/verify-blockchain.component.ts
+++ src/app/user-interface/verify-blockchain/verify-blockchain.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, HostListener, Output } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, HostListener, Output } from '@angular/core';
+import { BlockchainDataService } from '../services/blockchain-data.service';
 
 @Component({
   selector: 'app-verify-blockchain',
@@ -7,8 +8,33 @@ import { Component, EventEmitter, HostListener, Output } from '@angular/core';
   templateUrl: './verify-blockchain.component.html',
   styleUrl: './verify-blockchain.component.scss'
 })
-export class VerifyBlockchainComponent {
+export class VerifyBlockchainComponent implements AfterViewInit {
+  blockchain: any = [];
   @Output() userChoiceChange = new EventEmitter<string>();
+
+  constructor(private blockchainDataService: BlockchainDataService) {}
+
+  ngOnInit() {
+    this.blockchain = this.blockchainDataService.blockchain;
+  }
+
+  ngAfterViewInit(): void {
+    this.hackBlockchain();
+  }
+
+  hackBlockchain() {
+    this.displayBlockchainBeforeHack();
+    this.blockchain[0] = [2];
+    this.displayBlockchainAfterHack();
+  }
+
+  displayBlockchainBeforeHack() {
+
+  }
+
+  displayBlockchainAfterHack() {
+    
+  }
 
   @HostListener('document:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
