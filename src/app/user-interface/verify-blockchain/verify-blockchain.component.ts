7b93a0a317a7b97999f3490ae2eb72eb56cd36dd
Christoph <c.wagner.germany@gmail.com>
2024-04-17 17:47:59 +0200

    verify blockchain refactored

--- src/app/user-interface/verify-blockchain/verify-blockchain.component.ts
+++ src/app/user-interface/verify-blockchain/verify-blockchain.component.ts
@@ -30,8 +30,8 @@ export class VerifyBlockchainComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     setTimeout(() => {
-      this.hackBlockchain();
       this.verifyBlockchain();
+      this.hackBlockchain();
     });
   }
 
@@ -42,23 +42,45 @@ export class VerifyBlockchainComponent implements AfterViewInit {
   }
 
   verifyBlockchain() {
-    for (let blogIndex = 0; blogIndex < this.blockchain.length; blogIndex++) {
-      console.log(blogIndex);
-      console.log(this.blockchain[blogIndex][0]);
-      console.log(this.blockchain[blogIndex - 1]);
-
-      if (blogIndex === 0) {
-        continue;
-      } else if (
-        this.blockchain[blogIndex][0] == this.blockchain[blogIndex - 1]
-      ) {
-        console.log('Blockchain valid');
-      } else {
-        console.log('Blockchain invalid / hacked');
-        break;
-      }
+    if (this.isBlockchainValid()) {
+      console.log('Blockchain valid');
+    } else {
+      console.log('Blockchain invalid / hacked');
     }
   }
+
+  isBlockchainValid(): boolean {
+    if (this.blockchain.length === 0) return true;
+    for (let index = 1; index < this.blockchain.length; index++) {
+      if (!this.isLinkValid(index)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  isLinkValid(index: number): boolean {
+    return this.blockchain[index][0] === this.blockchain[index - 1];
+  }
+
+  // verifyBlockchain() {
+  //   for (let blogIndex = 0; blogIndex < this.blockchain.length; blogIndex++) {
+  //     console.log(blogIndex);
+  //     console.log(this.blockchain[blogIndex][0]);
+  //     console.log(this.blockchain[blogIndex - 1]);
+
+  //     if (blogIndex === 0) {
+  //       continue;
+  //     } else if (
+  //       this.blockchain[blogIndex][0] == this.blockchain[blogIndex - 1]
+  //     ) {
+  //       console.log('Blockchain valid');
+  //     } else {
+  //       console.log('Blockchain invalid / hacked');
+  //       break;
+  //     }
+  //   }
+  // }
 
   @HostListener('document:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
