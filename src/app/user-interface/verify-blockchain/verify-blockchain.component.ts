b6676423558b82092c1cfaa20e471cc806ed2092
Christoph <c.wagner.germany@gmail.com>
2024-04-17 17:49:08 +0200

    verify blockchain refactored3

--- src/app/user-interface/verify-blockchain/verify-blockchain.component.ts
+++ src/app/user-interface/verify-blockchain/verify-blockchain.component.ts
@@ -30,8 +30,8 @@ export class VerifyBlockchainComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     setTimeout(() => {
-      this.verifyBlockchain();
       this.hackBlockchain();
+      this.verifyBlockchain();
     });
   }
 
